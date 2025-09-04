d555165460a789d1aa5cd61c8cbab12891f21f2a
WilkerSebastian <wilkerafonso2005@gmail.com>
2025-09-04 16:22:25 -0400

    refactor: refazendo os endpoints relacionados a jogada do player no websocket para estrutura de controller, router, use-case

---
+++ src/application/use-cases/GamePlayer/deny-skill.case.ts
@@ -0,0 +1,21 @@
+import { SkillUseDTO } from "@/application/dto/skill.dto"
+import { Room } from "@/domain/aggregates/Room"
+import { RoomRepository } from "@/infrastructure/database/Memory/repositories/Room.repository"
+import { estadoHab } from "@/infrastructure/shared/estadoHab"
+
+export const denySkill = (obj: SkillUseDTO, update: (room: Room) => void) => {
+
+    console.log(obj)
+
+    const orm = new RoomRepository()
+    
+    const room = orm.getByID(obj.salaID)
+    
+    room.game.setEstadoHabilidade(estadoHab.inativa)
+    room.game.passarVez()
+    
+    update(room)
+
+    room.game.setHabilidade(-1, obj.playerID)
+
+}
---
+++ src/application/use-cases/GamePlayer/send-skill.case.ts
@@ -0,0 +1,15 @@
+import { RoomRepository } from "@/infrastructure/database/Memory/repositories/Room.repository"
+import { estadoHab } from "@/infrastructure/shared/estadoHab"
+
+export const sendSkill = (roomID: string, hab: any) => {
+
+    const orm = new RoomRepository()
+
+    const room = orm.getByID(roomID)
+
+    room.game.setEstadoHabilidade(estadoHab.aplicar)
+    room.game.aplicarHabilidade(hab)
+
+    return room
+
+}
